1f3895abde421943642bed2a12a9ca0d4126965e
Dane Grant <danecando@gmail.com>
2015-05-30 17:44:40 -0400

    remove pretest

--- build/protil.js
+++ build/protil.js
@@ -1,34 +1,40 @@
 /**
- *
+ * protil
+ * a utility library for working with promises
  */
 
 /*global define*/
 !(function (global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], function () {
-            return factory(global);
-        });
-    } else if (typeof exports === 'object') {
-        module.exports = factory(global);
-    } else {
-        global.noob = factory(global);
-    }
+  if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return factory(global);
+    });
+  } else if (typeof exports === 'object') {
+    module.exports = factory(global);
+  } else {
+    global.protil = factory(global);
+  }
 })(this, function (global) {
-    'use strict';
+  'use strict';
 
-    var protil = {};
+  var protil = {};
+  var Promise = Promise;
 
-    var prevProtil = undefined;
-    if (global.protil) {
-        prevProtil = global.protil;
-    }
+  var prevProtil = undefined;
+  if (global.protil) prevProtil = global.protil;
 
-    protil.noConflict = function noConflict() {
-        global.protil = prevProtil;
-        return protil;
-    };
+  // check for promise constructor
+  if (!Promise) {
+    var warning = 'Promise constructor not detected. ' + 'Please pass your promise implementation to the setPromise method.';
+    console.warn(warning);
+  }
 
-    return protil;
+  protil.noConflict = function noConflict() {
+    global.protil = prevProtil;
+    return this;
+  };
+
+  return protil;
 });
 
 //# sourceMappingURL=protil.js.map

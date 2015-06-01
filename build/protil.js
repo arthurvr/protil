879e412d8894c2ce508b4b03a47045ade020600d
Dane Grant <danecando@gmail.com>
2015-06-01 10:58:44 -0400

    fixed boilerplate: dont nodes global, remove requirejs

--- build/protil.js
+++ build/protil.js
@@ -3,61 +3,36 @@
  * a utility library for working with promises
  */
 
-/*global define*/
-!(function (global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], function () {
-      return factory(global);
-    });
-  } else if (typeof exports === 'object') {
+!(function (root, factory) {
+  if (typeof exports === 'object') {
     module.exports = factory(global);
+  } else if (root.window) {
+    root.protil = factory(root);
   } else {
-    global.protil = factory(global);
+    return;
   }
-})(this, function (global) {
+})(this, function (root) {
   'use strict';
 
   var protil = {};
-  console.log(Promise);
-  var Promise = global.Promise;
+  var Promise = root.Promise;
 
-  var prevProtil = undefined;
-  if (global.protil) prevProtil = global.protil;
+  var prev = undefined;
+  if (root.protil) prev = root.protil;
+
+  protil.noConflict = function () {
+    global.protil = prev;
+    return this;
+  };
 
   // check for promise constructor
-  if (!Promise) {
+  if (!Promise || typeof Promise !== 'function') {
     var warning = 'Promise constructor not detected. ' + 'Please pass your promise implementation to the setPromise method.';
     console.warn(warning);
   }
 
   protil.setPromise = function (p) {
     Promise = p;
-  };
-
-  protil.noConflict = function noConflict() {
-    global.protil = prevProtil;
-    return this;
-  };
-
-  protil.always = function (fn) {
-    return function () {
-      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-        args[_key] = arguments[_key];
-      }
-
-      var res = fn.apply(null, args);
-      function handle(error, value) {
-        return {
-          value: value,
-          rejected: error
-        };
-      }
-
-      if (!res.then) {
-        return Promise.resolve(handle(false, res));
-      }
-      return res.then(handle.bind(null, false), handle.bind(null, true));
-    };
   };
 
   return protil;
--- test/test.js
+++ test/test.js
@@ -5,26 +5,3 @@ test('export protil object', function(t) {
     t.equal(typeof protil, 'object');
     t.end();
 });
-
-test('protil.always', function(t){
-    var always = protil.always;
-    t.plan(5);
-    always(function(a,b){
-        return new Promise(function(r,reject){
-            t.notEquals(a,b, "Should pass down args correctly");
-            return reject('test');
-        })
-    })(1,2)
-    .then(function(data){
-        t.ok(data.rejected, "Should be flagged as rejected");
-        t.equals(data.value, 'test', "Should capture the reject reason");
-    });
-
-    always(function(){
-        return "test";
-    })()
-    .then(function(data){
-        t.notOk(data.rejected, "Should be flagged as resolved");
-        t.equals(data.value, 'test', "Should handle non promise values");
-    })
-})
